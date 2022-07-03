1468dd5ff07597649d973ebc6d2c729983610d8a
Fatima <xfatimakx@gmail.com>
2022-07-03 22:57:26 +0000

    tail.js exercise

---
+++ tail.js
@@ -0,0 +1,15 @@
+
+const tail = function (assertEqual.tail) {
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+    }
+  
+  };
+};
+
+  // TEST CODE
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  //assertEqual(1, 1);
