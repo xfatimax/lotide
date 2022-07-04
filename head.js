7a2e0df711dff60f563da62e5c62a4b096ad23eb
Fatima <xfatimakx@gmail.com>
2022-07-04 02:04:55 +0000

    Head.js with fixes

--- head.js
+++ head.js
@@ -1,15 +1,16 @@
 const assertEqual = function(actual, expected) {
-  const head = function () {
-    actual = head[0];
     if (actual === expected) {
       console.log(`✅  Assertion passed: ${actual} === ${expected}`);
     } else {
       console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
     }
-  }
 };
 
+const head = function(array) {
+  return array[0];
+};
 
+module.exports = head;
 
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
