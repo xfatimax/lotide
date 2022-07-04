a9d8af03846a465e53c03b2b6550391e6edd0268
Fatima <xfatimakx@gmail.com>
2022-07-04 03:19:27 +0000

    assertArraysEqual.js exercise

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+
+
+function eqArrays(arr1, arr2) {
+  if(arr1.length != arr2.length) {
+      return false;
+  } 
+  else { 
+      let result = false;
+      for(let i=0; i <arr1.length; i++) {
+
+          if(arr1[i] != arr2[i]) {
+              return false;
+          }
+          else {
+              result = true;
+          }
+      }
+      if (result === true) {
+        console.log("The arrays are equal"); }
+      else {
+        console.log("The arrays are not equal"); 
+      }
+  }
+ 
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); 
