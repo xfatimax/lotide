bf6de67c445e1e6d92be6946c44475e7f5bcebaa
Fatima <xfatimakx@gmail.com>
2022-07-04 03:02:05 +0000

    eqArrays.js exercise

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+//const assertEqual = require('./assertEqual');
+
+function eqArrays(arr1, arr2) {
+  if(arr1.length != arr2.length) {
+      return false;
+  } 
+  else { 
+      let result = false;
+      for(let i=0; i <arr1.length; i++) {
+
+          if(arr1[i] != arr2[i]) {
+              return false;
+          }
+          else {
+              result = true;
+          }
+      }
+      return result; 
+  }
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
