e2ac4f2be3d6892a4abb0c696c804591f2ffe752
Fatima <xfatimakx@gmail.com>
2022-07-08 02:36:01 +0000

    countOnly.js Final

--- countOnly.js
+++ countOnly.js
@@ -10,7 +10,7 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  const results = {};
+  const results = [];
 
   for (const item of allItems) {
     if (results[item]) {
@@ -18,14 +18,7 @@ const countOnly = function(allItems, itemsToCount) {
     } else {
       results[item] = 1;
     }
-    console.log(item);
-    if (itemsToCount[item]) { 
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
-    }
+    //console.log(item);
   }
 
   return results;
