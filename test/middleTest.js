dbe3a9ca991ca7736ab9b7122c56dd538ae16e0f
Fatima <xfatimakx@gmail.com>
2022-08-02 01:34:43 +0000

    Lotide project with small updates

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,20 @@
 
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+
+// assertArraysEqual(middle([1, 2, 3]), 2); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 6]), 3); // => [3, 4]
+
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3]), 2); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 6]), 3); // => [3, 4]
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns '3,4' for ['1,2,3,4,5,6']", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [ 3, 4 ]); 
+  });
+
+});
